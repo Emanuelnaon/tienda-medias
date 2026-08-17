330f2a81b584b5ebeee2bd7b6150c541769f13ec
Emanuel Naon <emanuelnaon@gmail.com>
2026-08-17 05:35:29 -0300

    import de tablaprodictos

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { GrillaProductos } from '@/src/features/catalogo/components/GrillaProductos';
-
+import {TablaProductos} from '@/src/features/admin/components/TablaProductos';
 export default async function Home({ searchParams }: { searchParams: { [key: string]: string | string[] | undefined } }) {
   const params = await searchParams; 
   return <GrillaProductos parametros={params} />;
